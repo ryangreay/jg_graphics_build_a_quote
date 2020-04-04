ac702aed5fd53b5b98e2fa76de52fc9e83df3635
ryangreay <rgray003@ucr.edu>
2020-04-04 13:46:34 -0700

    1 file modified, 7 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { StepContentComponent } from './step-content/step-content.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HeaderNavComponent, StepContentComponent, ProductViewComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/step-content/step-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-content',
+  templateUrl: './step-content.component.html',
+  styleUrls: ['./step-content.component.css']
+})
+export class StepContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

fbde9e301b8333e9f71513017e7a4b0245896fea
ryangreay <rgray003@ucr.edu>
2020-04-05 17:38:09 -0700

    1 file modified

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -4,12 +4,10 @@ import { WizardService } from '../wizard.service';
 @Component({
   selector: 'app-header-nav',
   templateUrl: './header-nav.component.html',
-  styleUrls: ['./header-nav.component.css'],
-  providers: [WizardService]
+  styleUrls: ['./header-nav.component.css']
 })
 export class HeaderNavComponent implements OnInit {
   stepNumber: number;
-  currentProduct: string;
   nextDisabled: boolean;
   stepLabel: string;
   private wizard: WizardService;
@@ -23,13 +21,12 @@ export class HeaderNavComponent implements OnInit {
   }
 
   nextIsDisabled(){
-    /getWizardValues();
-    //alert(this.wizard.getProductSelection());
     return this.wizard.nextStepDisabled();
   }
 
   navigateToNextStep(){
     this.wizard.setCurrentStep(this.stepNumber + 1);
+    alert(this.wizard.getCurrentStep)
     this.getWizardValues();
   }
 
@@ -42,7 +39,6 @@ export class HeaderNavComponent implements OnInit {
     this.stepNumber = this.wizard.getCurrentStep();
     this.nextDisabled = this.wizard.nextStepDisabled();
     this.stepLabel = this.wizard.getStepLabel();
-    this.currentProduct = this.wizard.getProductSelection();
   }
 
 }
